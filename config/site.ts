e8345a55373c05cccda90b57b2e82fe2b20365af
ego <mrego@anakonn.com>
2024-08-30 12:48:36 +0900

    site config

--- config/site.ts
+++ config/site.ts
@@ -1,8 +1,8 @@
 export const siteConfig = {
-	title: 'DocsForAll-Sub Template',
+	title: 'DocsForAll-Vim',
 	url: 'https://docsforall.com/',
-	base: '/sub-template',
+	base: '/vim',
 	googleAdsSrc:
 		'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2130210715518535',
-	githubUrl: 'https://github.com/Anakonn/docsforall-sub-template',
+	githubUrl: 'https://github.com/Anakonn/vim-docs',
 };
