bee659884b61f8aa3452f49db6eeededef1c249a
ego <mrego@anakonn.com>
2024-08-30 17:37:17 +0900

    add base

--- config/site.ts
+++ config/site.ts
@@ -1,6 +1,7 @@
 export const siteConfig = {
 	title: 'Vim',
 	url: 'https://docsforall.com/',
+	base: '/vim',
 	googleAdsSrc:
 		'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2130210715518535',
 	githubUrl: 'https://github.com/Anakonn/vim-docs',
