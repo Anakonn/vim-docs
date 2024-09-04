6b219f91876cf7f46391723cfef425049cef3e04
ego <mrego@anakonn.com>
2024-09-04 16:17:28 +0900

    change site url

--- config/site.ts
+++ config/site.ts
@@ -1,6 +1,6 @@
 export const siteConfig = {
 	title: 'Vim',
-	url: 'https://docsforall.com/',
+	url: 'https://vim.docsforall.com/',
 	googleAdsSrc:
 		'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2130210715518535',
 	githubUrl: 'https://github.com/Anakonn/vim-docs',
